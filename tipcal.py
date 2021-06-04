5b6a79830c71a8315bbe49333fe376ce45094004
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-06-04 23:42:00 +0100

    added comments....

--- tipcal.py
+++ tipcal.py
@@ -2,26 +2,29 @@ from tkinter import Tk, Radiobutton, Button, Label, StringVar, IntVar, Entry
 
 class TipCalculator():
     def __init__(self):
-        window = Tk()
-        window.title("Tip Calculator App")
-        window.configure(background = "dark grey")
-        window.geometry("375x250")
-        window.resizable(width = False, height = False)
+        window = Tk()                                 # create application window                  
+        window.title("Tip Calculator App")            # add title to application window
+        window.configure(background = "dark grey")    # Add background color
+        window.geometry("375x250")                     # specify size of the window
+        window.resizable(width = False, height = False) # fixed the width and height size
         
+        # declaring our variables
         self.meal_cost = StringVar()
         self.tip_percent = IntVar()
         self.tip = StringVar()
         self.total_cost = StringVar()
 
+        # create the tip label
         tip_percents = Label(window, text = "Tip Percentage", bg = "Purple", fg = "white")
         tip_percents.grid(column = 0, row = 0, padx = 15)
 
+        # Create the bill amount label and entry function
         bill_amount = Label(window, text = "Bill Amount", bg = "black", fg = "white")
         bill_amount.grid(column = 1, row = 0, padx = 15)
         bill_amount_entry = Entry(window, textvariable = self.meal_cost, width = 14)
         bill_amount_entry.grid(column = 2, row = 0)
 
-        
+        # create all the percent tip radio button
         five_percent_tip = Radiobutton(window, text = " 5% ", variable = self.tip_percent, value = 5)
         five_percent_tip.grid(column = 0, row = 1 )
         ten_percent_tip = Radiobutton(window, text = "10%", variable = self.tip_percent, value = 10)
@@ -35,20 +38,23 @@ class TipCalculator():
         thirty_percent_tip = Radiobutton(window, text = "30%", variable = self.tip_percent, value = 30)
         thirty_percent_tip.grid(column = 0, row = 6 )
 
-        
+        # create tip amount label and entry function
         tip_ammount_lbl = Label(window, text = "Tip Amount", bg = "brown", fg = "white")
         tip_ammount_lbl.grid(column = 1, row = 3, padx = 15)
         tip_amount_entry = Entry(window, textvariable = self.tip, width = 14)
         tip_amount_entry.grid (column = 2, row = 3)
 
+        #create bill total label and entry function
         bill_total_lbl = Label(window, text = "Bill Total", bg = "blue", fg = "white")
         bill_total_lbl.grid(column = 1, row = 5, padx = 15)
         bill_total_entry = Entry(window, textvariable = self.total_cost, width = 14)
         bill_total_entry.grid (column = 2, row = 5)
 
+        # create calculate button
         calculate_btn = Button(window, text = "Calculate", bg = "green", fg = "white", command = self.calculate)
         calculate_btn.grid(column = 1, row = 7, padx = 15)
 
+        # create the clear buttin
         clear_btn = Button(window, text = "Clear", bg = "black", fg = "white", command = self.clear)
         clear_btn.grid(column = 2, row = 7)
 
